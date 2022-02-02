62285d031646306d468a00f3aa75312c0b3786bd
Danny Joseph <89165878+DevByDJ@users.noreply.github.com>
2022-02-02 00:25:15 -0500

    FINAL BUILD
    HW1 Assignment for COP 2001

---
+++ COP 2001/HW1/HW1.cpp
@@ -0,0 +1,20 @@
+/*---------------------------------------------------------
+ *    File: main.cpp
+ *  Author: Danny Joseph
+ *   Class: COP 2001, Spring 2022
+ * Purpose: Hello World program to familiarize self with IDE
+ * ---------------------------------------------------------*/
+
+#include <iostream> 
+#include <string>       // create a string variable for my name 
+
+using namespace std;
+
+int main() {
+
+    string myName = "Danny Joseph";
+
+    cout << "Hello, World!" << " my name is " + myName << endl;
+
+    return 0;
+}
