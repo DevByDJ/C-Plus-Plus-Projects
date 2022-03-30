53776cd44dd6f7535718c6d89af4751b92fc9f2d
Danny Joseph <89165878+DevByDJ@users.noreply.github.com>
2022-03-30 14:13:29 -0400

    v3.0
    Completed Project!

--- Hellofml/moveball.cpp
+++ Hellofml/moveball.cpp
@@ -26,11 +26,22 @@ const float SPEED_TIME = (3.0 * 1000.0) * 30.0;     //
 const float SPEED_X = WINDOW_WIDTH / SPEED_TIME;    // speed horizontally
 const float SPEED_Y = WINDOW_HEIGHT / SPEED_TIME;   // span  vertically
 
+// wall properties
+const float WALL_THICKNESS = 10.0;
+const sf::Color WALL_COLOR = sf::Color::Blue;
 
 // Type definitions
 // --------------------------------------------------------
 
 // <direction enum here>
+enum Direction{
+    Exit = -1,
+    None,
+    Left,
+    Up,
+    Right,
+    Down
+};
 
 // ball properties
 struct Ball {
@@ -43,20 +54,29 @@ struct Ball {
     sf::Color color;
 };
 
-enum Direction{
-    Exit = -1,
-    None,
-    Left,
-    Up,
-    Right,
-    Down
+// wall properties
+struct Wall {
+    float leftSide;
+    float topSide;
+    float widthOfWall;
+    float heightOfWall;
+    sf::Color wallColor;
+    sf::RectangleShape wallShape;
+};
+
+// border properties
+struct Border {
+    Wall leftWall;
+    Wall topWall;
+    Wall rightWall;
+    Wall bottomWall;
 };
 
 // Function declarations (prototypes)
 // --------------------------------------------------------
 Direction processInput() ;
-void update(Direction &input, Ball ball, float delta);
-void render(sf::RenderWindow &window, Ball ball, float delta);
+void update(Direction &input, Ball &ball, float delta);
+void render(sf::RenderWindow &window, Ball &ball, float delta, Border border);
 
 
 
@@ -67,7 +87,7 @@ void render(sf::RenderWindow &window, Ball ball, float delta);
 int main() {
 
     // create a 2d graphics window
-    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Hello SFML");
+    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Move Ball");
     window.clear(WINDOW_COLOR);
 
     // declare a ball variable and populate it in the center of the window
@@ -85,6 +105,9 @@ int main() {
     sf::Time startTime = clock.getElapsedTime();
     sf::Time stopTime = startTime;
     float delta = 0.0;
+
+    // Border variable
+    Border theBorders;
 
     bool gameOver = false;
     while (!gameOver)
@@ -122,7 +145,7 @@ int main() {
 
         // Render the window
         // ------------------------------------------------
-        render(window, theBall, delta);
+        render(window, theBall, delta, theBorders);
 
     } // end main game loop
 
@@ -136,7 +159,7 @@ int main() {
 /**
  * convert user keyboard input into recognized integer values
  * for left=1/up=2/right=3/down=4
- * @return Direction - user input (default no-input=None, quit=Exit)
+ * @return int - user input (default no-input=0, quit=-1)
  */
 Direction processInput() {
     Direction input = None;  // no input
@@ -171,7 +194,7 @@ Direction processInput() {
  * @param ball  - update ball position and speed
  * @param delta - current frame time
  */
-void update(Direction &input, Ball ball, float delta) {
+void update(Direction &input, Ball &ball, float delta) {
 
     // adjust velocity directions for user input
     if (input) {
@@ -211,11 +234,81 @@ void update(Direction &input, Ball ball, float delta) {
  * @param ball   - structure variable with properties for the ball
  * @param delta  - amount of frame time plus lag (in ms)
  */
-void render(sf::RenderWindow &window, Ball ball, float delta) {
+void render(sf::RenderWindow &window, Ball &ball, float delta, Border border) {
     // Render drawing objects
     // ------------------------------------------------
     // clear the window with the background color
     window.clear(WINDOW_COLOR);
+
+    // draw the border
+    //-------------------------------------------------
+    Border newBorder;
+
+    // left wall properties
+    // initialize the left and top side values
+    newBorder.leftWall.leftSide = 0.0;
+    newBorder.leftWall.topSide = 0.0;
+    // set the size of the wall based on const variables, WALL THICKNESS and WINDOW HEIGHT
+    newBorder.leftWall.heightOfWall = WALL_THICKNESS;
+    newBorder.leftWall.widthOfWall = WINDOW_WIDTH;
+    newBorder.leftWall.wallShape.setSize(sf::Vector2f(newBorder.leftWall.widthOfWall,
+                                                      newBorder.leftWall.heightOfWall));
+    // set position of left wall within the window using left and top side values.
+    newBorder.leftWall.wallShape.setPosition(sf::Vector2f(newBorder.leftWall.leftSide,
+                                                          newBorder.leftWall.topSide));
+    // set the color of the wall to the constant value, WALL_COLOR
+    newBorder.leftWall.wallShape.setFillColor(WALL_COLOR);
+    window.draw(newBorder.leftWall.wallShape);
+
+    // top wall properties
+    // initialize the left and top side values
+    newBorder.topWall.leftSide = 0.0;
+    newBorder.topWall.topSide = 0.0;
+    // set the size of the wall based on const variables, WALL THICKNESS and WINDOW HEIGHT
+    newBorder.topWall.heightOfWall = WINDOW_HEIGHT;
+    newBorder.topWall.widthOfWall = WALL_THICKNESS;
+    newBorder.topWall.wallShape.setSize(sf::Vector2f(newBorder.topWall.widthOfWall,
+                                                     newBorder.topWall.heightOfWall));
+    // set position of left wall within the window using left and top side values.
+    newBorder.topWall.wallShape.setPosition(sf::Vector2f(newBorder.topWall.leftSide,
+                                                         newBorder.topWall.topSide));
+    // set the color of the wall to the constant value, WALL_COLOR
+    newBorder.topWall.wallShape.setFillColor(WALL_COLOR);
+    window.draw(newBorder.topWall.wallShape);
+
+    // right wall properties
+    // initialize the left and top side values
+    newBorder.rightWall.leftSide = WINDOW_WIDTH - WALL_THICKNESS;
+    newBorder.rightWall.topSide = 0.0;
+    // set the size of the wall based on const variables, WALL THICKNESS and WINDOW HEIGHT
+    newBorder.rightWall.heightOfWall = WINDOW_HEIGHT;
+    newBorder.rightWall.widthOfWall = WALL_THICKNESS;
+    newBorder.rightWall.wallShape.setSize(sf::Vector2f(newBorder.rightWall.widthOfWall,
+                                                       newBorder.rightWall.heightOfWall));
+    // set position of left wall within the window using left and top side values.
+    newBorder.rightWall.wallShape.setPosition(sf::Vector2f(newBorder.rightWall.leftSide,
+                                                           newBorder.rightWall.topSide));
+    // set the color of the wall to the constant value, WALL_COLOR
+    newBorder.rightWall.wallShape.setFillColor(WALL_COLOR);
+    window.draw(newBorder.rightWall.wallShape);
+
+    // bottom wall properties
+    // initialize the left and top side values
+    newBorder.bottomWall.leftSide = 0.0;
+    newBorder.bottomWall.topSide = WINDOW_HEIGHT - WALL_THICKNESS;
+    // set the size of the wall based on const variables, WALL THICKNESS and WINDOW HEIGHT
+    newBorder.bottomWall.heightOfWall = WALL_THICKNESS;
+    newBorder.bottomWall.widthOfWall = WINDOW_WIDTH;
+    newBorder.bottomWall.wallShape.setSize(sf::Vector2f(newBorder.bottomWall.widthOfWall,
+                                                        newBorder.bottomWall.heightOfWall));
+    // set position of left wall within the window using left and top side values.
+    newBorder.bottomWall.wallShape.setPosition(sf::Vector2f(newBorder.bottomWall.leftSide,
+                                                            newBorder.bottomWall.topSide));
+    // set the color of the wall to the constant value, WALL_COLOR
+    newBorder.bottomWall.wallShape.setFillColor(WALL_COLOR);
+    window.draw(newBorder.bottomWall.wallShape);
+
+
 
     // draw the ball
     // ------------------------------------------------
@@ -229,6 +322,7 @@ void render(sf::RenderWindow &window, Ball ball, float delta) {
     float yPosition = ball.coordinateY + ball.velocityY * delta;
     circle.setPosition(xPosition, yPosition);
     window.draw(circle);
+
 
     // show the new window
     // ------------------------------------------------
