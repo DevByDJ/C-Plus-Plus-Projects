7ec1cd1e876069a8a5dedb7980f2975cabce4d4f
DevByDJ <DjosephNP@gmail.com>
2022-09-09 20:51:46 -0400

    Matrix Solution Updated

--- COP 3003/matrixMultiplication/matrixMulti.cpp
+++ COP 3003/matrixMultiplication/matrixMulti.cpp
@@ -7,22 +7,27 @@ using namespace std;
 
 int main() 
 {
-  int matrix1[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-  int matrix2[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+  int matrixOne[3][3] = {{7, 13, 30}, {2, 12, 61}, {6, 9, 68}};
+  int matrixTwo[3][3] = {{6, 17, 3}, {11, 26, 5}, {1, 8, 9}};
   int results[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
 
   for (int i = 0; i < 3; i++)
       for (int j = 0; j < 3; j++) {
           for (int u = 0; u < 3; u++)
-              results[i][j] += matrix1[i][u] * matrix2[u][j];
+              results[i][j] += matrixOne[i][u] * matrixTwo[u][j];
       }
 
-  cout << endl << "Output Matrix: " << endl;
+  cout << endl << "Resulting Matrix: " << endl << endl;
 
   for (int i = 0; i < 3; i++) {
       for (int j = 0; j < 3; j++) {
           cout << results[i][j] << ".";
       }
-      cout << endl;
+      cout << endl; 
+  }
+
+  cout << endl;
+
+  return 0;
   
 }
