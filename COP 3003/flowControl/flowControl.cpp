589b43fcaa31efa8908c1a89c76793a9203eac7b
DevByDJ <DjosephNP@gmail.com>
2022-09-09 20:21:29 -0400

    new flow control

---
+++ COP 3003/flowControl/flowControl.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+
+int main() 
+{
+  int solution;
+
+  for(int i = 0; i < 6 ; i++)
+  {
+    solution = 5 * i;
+    cout << "5 x " << i << " = " << solution << endl;
+  }
+
+  return 0;
+  
+}
