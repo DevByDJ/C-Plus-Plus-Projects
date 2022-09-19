5855567971837a2699d9c0c13548f2b49a1fe1b7
DevByDJ <DjosephNP@gmail.com>
2022-09-19 13:21:16 -0400

    Updating file structures

---
+++ COP 3003/InClass Assignments/Week5/Week5.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+using namespace std;
+
+class Room 
+{
+    public:
+        double length, width, height;
+
+        void getArea() {
+            double area;
+
+            area = length * width;
+
+            cout << "The area for this room is " << area << " feet" << endl;
+
+        };
+
+        void getVolume() {
+            double volume;
+
+            volume = length * width * height;
+
+            cout << "The volume for this room is " << volume << " feet" << endl;
+
+        };
+
+};
+
+
+int main () 
+{
+
+    Room bedRoom, classRoom, diningRoom;
+
+    bedRoom.width = 10.5;
+    bedRoom.height = 12.2;
+    bedRoom.length = 8.8;
+
+    classRoom.width = 12.3;
+    classRoom.height = 11.4;
+    classRoom.length = 15.9;
+
+    diningRoom.width = 13.3;
+    diningRoom.height = 10.1;
+    diningRoom.length = 7.8;
+
+    bedRoom.getArea();
+    bedRoom.getVolume();
+
+    classRoom.getArea();
+    classRoom.getVolume();
+
+    diningRoom. getArea();
+    diningRoom.getVolume();    
+
+
+    return 0;
+
+}   
