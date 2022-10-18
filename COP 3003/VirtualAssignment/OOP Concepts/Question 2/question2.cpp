21d422715944a3c61e3d8adca5d161ec118afc0b
Danny Joseph <89165878+DevByDJ@users.noreply.github.com>
2022-10-18 12:11:08 -0400

    Question 2 Completed and file management completed

---
+++ COP 3003/VirtualAssignment/OOP Concepts/Question 2/question2.cpp
@@ -0,0 +1,62 @@
+/*
+* Write a program to find the factorial of a number using methods outside 
+* the class
+**/
+
+#include<iostream>
+
+using namespace std;
+
+class UserInput
+{
+    public:
+
+    // -- Variable Declaration --
+    double number;
+
+    double getNumber()
+    {
+        // -- INPUT --
+        cout << "Enter a number: ";
+        cin >> number;
+
+        return number;
+    };
+
+};
+
+double factorialOfNumber(double number);
+
+int main()
+{
+    // -- OBJECT & VARIABLE DECLARATION --
+    UserInput userInput;
+    double number, result;
+
+    // -- INPUT USING THE CLASS METHOD --
+    number = userInput.getNumber();
+
+    // -- CALCULATIONS USING METHOD OUTSIDE CLASS --
+    result = factorialOfNumber(number);
+
+    // -- OUTPUT --
+    cout << endl << "The result of " << number << " factorial is = " << result << endl;
+
+    return 0;
+}
+
+double factorialOfNumber(double number)
+{
+    // As long as the number entered isn't one do this..
+    if(number > 1)
+    {
+        //  N x (N - 1) ..
+        return number * factorialOfNumber(number - 1);
+    }
+    else
+    {
+        // Return 1 if the user chooses any factorial less than 1
+        return 1;
+    }
+
+};
